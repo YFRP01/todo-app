2ea1b0f8afb6861bda261e865ee8c6cf346a7aec
Youmsi Pavel <rahilpavel533@gmail.com>
2026-02-01 00:34:53 +0800

    Clean up db.js by removing commented code
    Removed commented-out database connection configuration.

--- backend/db.js
+++ backend/db.js
@@ -2,14 +2,6 @@ import { Pool } from "pg";
 import dotenv from "dotenv";
 dotenv.config();
 
-// const pool = new Pool({
-//   user: "postgres",
-//   password: "Postgresql237@.",
-//   port: 5432,
-//   database: "tododb"
-// })
-
-
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl:
